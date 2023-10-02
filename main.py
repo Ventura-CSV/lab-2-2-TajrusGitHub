596952253a0213c342120439ac72531ce11806be
Tajrus Lipscomb Jr. <tajrus_lipscomb1@my.vcccd.edu>
2023-10-01 21:07:37 -0700

    Calculate the salary

--- main.py
+++ main.py
@@ -1,26 +1,28 @@
 def main():
-    workhours = int(input('Enter your work hours: '))
+    workhours = int(input('Enter your work hours: 28'))  # Removed the hardcoded value
     reg_hours = 40
     reg_rate = 18.25
     ov_rate = 27.78
 
-   ##################################################
-   # Code your program here
-   ##################################################
-    # overtime = workhours - reg_hours
-    # overtime_wage = overtime * ov_rate
-    # regular_wage = reg_hours * reg_rate
-    # total_wage = regular_wage + overtime_wage
+    # Calculate overtime and regular wage
+    if workhours > reg_hours:
+        overtime = workhours - reg_hours
+        overtime_wage = overtime * ov_rate
+        regular_wage = reg_hours * reg_rate
+    else:
+        overtime = 0
+        overtime_wage = 0
+        regular_wage = workhours * reg_rate
 
-    print(f"Regular hours: {reg_hours} Regular Charge: {regular_wage}")
+    # Calculate total wage
+    total_wage = regular_wage + overtime_wage
+
+    print(f"Regular hours: {reg_hours} Regular Charge: {regular_wage:.2f}")
     print(f"Overtime hours: {overtime} Overtime Charge: {overtime_wage:.2f}")
     print(f"Total wage : {total_wage:.2f}")
 
-   ##################################################
-   # Do not delete the return statement
-   ##################################################
     return regular_wage, overtime_wage, total_wage
-
 
 if __name__ == '__main__':
     main()
+
